d58c9501caa5c66986ba9bc4ac0c89b2ab589891
techme <techme@live.cn>
2016-06-14 14:52:24 +0800

    ？为啥会这样

--- HubCommit/HubCommit/MainWindow.xaml.cs
+++ HubCommit/HubCommit/MainWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace HubCommit
         /// <param name="e"></param>
         private void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Enter)
+            if (e.Key == System.Windows.Input.Key.Enter && datepicker.SelectedDate!=null)
             {
                 DateTime t = DateTime.Now;
                 SYSTEMTIME stimt = new SYSTEMTIME();
@@ -61,63 +61,66 @@ namespace HubCommit
         /// <returns></returns>
         public bool changeCurrDateTime()
         {
-            DateTime t = DateTime.Now;
-            SYSTEMTIME st = new SYSTEMTIME();
-            st.FromDateAndTime(datepicker.SelectedDate.Value, t);
-            Win32API.SetLocalTime(ref st);
+            if (datepicker.SelectedDate != null)
+            {
+                DateTime t = DateTime.Now;
+                SYSTEMTIME st = new SYSTEMTIME();
+                st.FromDateAndTime(datepicker.SelectedDate.Value, t);
+                Win32API.SetLocalTime(ref st);
+            }
             //RunCmd("date "+ (datepicker.SelectedDate.HasValue?DateTime.Now.ToString("yyyy/MM/dd") :datepicker.SelectedDate.Value.ToString("yyyy/MM/dd")));
             return true;
         }
 
-        ///// <summary>
-        ///// 执行CMD语句，经测试win10下不能用，废弃
-        ///// </summary>
-        ///// <param name="cmd">要执行的CMD命令</param>
-        //public void RunCmd(string cmd)
-        //{
-        //    proc.StartInfo.CreateNoWindow = true;
-        //    proc.StartInfo.FileName = "cmd.exe";
-        //    proc.StartInfo.UseShellExecute = false;
-        //    proc.StartInfo.RedirectStandardError = true;
-        //    proc.StartInfo.RedirectStandardInput = true;
-        //    proc.StartInfo.RedirectStandardOutput = true;
-        //    proc.Start();
-        //    proc.StandardInput.WriteLine(cmd);
-        //    string output = proc.StandardOutput.ReadToEnd();
-        //    proc.WaitForExit();
-        //    proc.Close();
-        //}
+        /// <summary>
+        /// 执行CMD语句，经测试win10下不能用，废弃
+        /// </summary>
+        /// <param name="cmd">要执行的CMD命令</param>
+        public void RunCmd(string cmd)
+        {
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+            proc.StandardInput.WriteLine(cmd);
+            string output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            proc.Close();
+        }
 
-        ///// <summary>
-        ///// 打开软件并执行命令，经测试win10下不能用，废弃
-        ///// </summary>
-        ///// <param name="programName">软件路径加名称（.exe文件）</param>
-        ///// <param name="cmd">要执行的命令</param>
-        //public void RunProgram(string programName, string cmd)
-        //{
-        //    Process proc = new Process();
-        //    proc.StartInfo.CreateNoWindow = true;
-        //    proc.StartInfo.FileName = programName;
-        //    proc.StartInfo.UseShellExecute = false;
-        //    proc.StartInfo.RedirectStandardError = true;
-        //    proc.StartInfo.RedirectStandardInput = true;
-        //    proc.StartInfo.RedirectStandardOutput = true;
-        //    proc.Start();
-        //    if (cmd.Length != 0)
-        //    {
-        //        proc.StandardInput.WriteLine(cmd);
-        //    }
-        //    proc.Close();
-        //}
+        /// <summary>
+        /// 打开软件并执行命令，经测试win10下不能用，废弃
+        /// </summary>
+        /// <param name="programName">软件路径加名称（.exe文件）</param>
+        /// <param name="cmd">要执行的命令</param>
+        public void RunProgram(string programName, string cmd)
+        {
+            Process proc = new Process();
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = programName;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+            if (cmd.Length != 0)
+            {
+                proc.StandardInput.WriteLine(cmd);
+            }
+            proc.Close();
+        }
 
-        ///// <summary>
-        ///// 打开软件，经测试win10下不能用，废弃
-        ///// </summary>
-        ///// <param name="programName">软件路径加名称（.exe文件）</param>
-        //public void RunProgram(string programName)
-        //{
-        //    this.RunProgram(programName, "");
-        //}
+        /// <summary>
+        /// 打开软件，经测试win10下不能用，废弃
+        /// </summary>
+        /// <param name="programName">软件路径加名称（.exe文件）</param>
+        public void RunProgram(string programName)
+        {
+            this.RunProgram(programName, "");
+        }
 
         #endregion Method
     }
